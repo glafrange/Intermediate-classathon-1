805d5dfcfc03452b8c2e5552b98bb7bc8af1be93
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-09-23 23:24:38 -0400

    factorialize

---
+++ classathon-csharp/classathon-csharp/Factorializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classathon_csharp
+{
+	public class Factorializer
+	{
+		public static ulong Factorialize(int num)
+		{
+			if (num == 0)
+				return 1;
+			else
+				return (ulong)num * Factorialize(num - 1);
+		}
+	}
+}
---
+++ classathon-csharp/classathon-csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace classathon_csharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Factorializer.Factorialize(5));
+		}
+	}
+}
